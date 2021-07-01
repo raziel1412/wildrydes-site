fb3dc302c5404dcee78beebb1bb34787401b48a0
Anzen <raziel_alfor@hotmail.com>
2021-07-01 12:24:41 -0500

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
-        region: 'eu-west-1'
+        userPoolId: 'us-east-2_DXTvkles2',
+        userPoolClientId: '239mu45a7pajh4osn6v8kss4g',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
